a9e73427d9c0e2fd825dfd7e63c049b9a37e1d4d
anna <annjellyiu5@gmail.com>
2016-11-06 18:10:41 +0300

    запрет редиректа на ту же страницу через меню

--- Counter/app/reducers/route.js
+++ Counter/app/reducers/route.js
@@ -10,6 +10,7 @@ const initialState = {
 export default function (state = initialState, action={}) {
     switch (action.type) {
         case "PUSH_NEW_ROUTE":
+            console.log('push'+action.route)
             globalNav.navigator.push({id: action.route});
             return {
                 routes: [...state.routes, action.route]
@@ -25,19 +26,22 @@ export default function (state = initialState, action={}) {
         case "REPLACE_OR_PUSH_ROUTE":
         {
             let routes = state.routes;
-
-            if (routes[routes.length - 1] === 'home') {
-                if (action.route !== 'home') {
-                    globalNav.navigator.push({id: action.route});
-                } else { // If top route is home and user navigates to home, do nothing
-                    routes = [];
-                }
-            } else if (action.route === 'home') {
-                globalNav.navigator.resetTo({id: 'home'});
+            if (routes[routes.length - 1] == action.route) {
                 routes = [];
-            } else {
-                globalNav.navigator.replaceWithAnimation({id: action.route});
-                routes.pop();
+            }else {
+                if (routes[routes.length - 1] === 'home') {
+                    if (action.route !== 'home') {
+                        globalNav.navigator.push({id: action.route});
+                    } else { // If top route is home and user navigates to home, do nothing
+                        routes = [];
+                    }
+                } else if (action.route === 'home') {
+                    globalNav.navigator.resetTo({id: 'home'});
+                    routes = [];
+                } else {
+                    globalNav.navigator.replaceWithAnimation({id: action.route});
+                    routes.pop();
+                }
             }
 
             return {
